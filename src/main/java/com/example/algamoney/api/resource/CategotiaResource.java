6a05a71e965e5c61184d1697add3219471dbb2f3
Victor André <and_blah@hotmail.com>
2021-01-06 01:49:55 -0300

    criando metodo post de categoria

--- src/main/java/com/example/algamoney/api/resource/CategotiaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategotiaResource.java
@@ -3,9 +3,13 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.model.Categoria;
@@ -33,4 +37,11 @@ public class CategotiaResource {
 //		List<Categoria> categoria = categoriaRepository.findAll();
 //		return !categoria.isEmpty() ? ResponseEntity.ok(categoria) : ResponseEntity.noContent().build();
 //	}
+
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void criarCategoria(@RequestBody Categoria categoria) {
+//		Categoria categoriaSalva = 
+		categoriaRepository.save(categoria);
+	}
 }
