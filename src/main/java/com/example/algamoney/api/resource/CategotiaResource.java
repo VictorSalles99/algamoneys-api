a16888f84521e375b3ab25ac36a0cac201e212d2
Victor André <and_blah@hotmail.com>
2021-01-06 01:31:43 -0300

    criando outro metodo get all que manipula o status caso o content seja vazio

--- src/main/java/com/example/algamoney/api/resource/CategotiaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategotiaResource.java
@@ -3,6 +3,7 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,20 @@ import com.example.algamoney.api.repository.CategoriaRepository;
 @RequestMapping("/categorias")
 public class CategotiaResource {
 
-	//injeção de dependencia
+	// injeção de dependencia
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
-	//criando o metodo de buscar todas categorias
+
+	// criando o metodo de buscar todas categorias
 	@GetMapping
 	public List<Categoria> buscarTodos() {
 		return categoriaRepository.findAll();
 	}
+
+	// Metodo criado para poder manupilar o status de retorno
+//	@GetMapping()
+//	public ResponseEntity<?> buscarTodosTrocandoStatus() {
+//		List<Categoria> categoria = categoriaRepository.findAll();
+//		return !categoria.isEmpty() ? ResponseEntity.ok(categoria) : ResponseEntity.noContent().build();
+//	}
 }
