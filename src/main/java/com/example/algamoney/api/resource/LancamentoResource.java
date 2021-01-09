e6f79bd55ac2bb76ff5f87e2d48b6bc24bb957a6
Victor André <and_blah@hotmail.com>
2021-01-08 21:16:12 -0300

    add regra para nao salvar lancamentos de pessoas inativas

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api.resource;
 
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,8 +10,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,34 +23,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoEvents;
+import com.example.algamoney.api.exceptionhandler.ResponseExceptionHandler.Erro;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.service.LancamentoService;
+import com.example.algamoney.api.service.exception.PessoaInativaException;
+import com.example.algamoney.api.service.exception.PessoaInexistenteException;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamento")
 public class LancamentoResource {
 
 	@Autowired
+	private LancamentoService lancamentoService;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@Autowired
 	private ApplicationEventPublisher publisher;
-	
+
 	@Autowired
 	LancamentoRepository lancamentoRepository;
-	
+
 	@GetMapping
-	public List<Lancamento> buscarTodosLancamentos(){
+	public List<Lancamento> buscarTodosLancamentos() {
 		return lancamentoRepository.findAll();
 	}
-	
+
 	@GetMapping("/{codigo}")
-	public ResponseEntity<Lancamento> buscarLancamentoPorCodigo(@PathVariable Long codigo){
+	public ResponseEntity<Lancamento> buscarLancamentoPorCodigo(@PathVariable Long codigo) {
 		Optional<Lancamento> lancamentoEncontrado = lancamentoRepository.findById(codigo);
-		return !lancamentoEncontrado.isEmpty() ? ResponseEntity.ok(lancamentoEncontrado.orElse(null)) : ResponseEntity.notFound().build();
+		return !lancamentoEncontrado.isEmpty() ? ResponseEntity.ok(lancamentoEncontrado.orElse(null))
+				: ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<?> cadastrarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response){
-		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+	public ResponseEntity<?> cadastrarLancamento(@Valid @RequestBody Lancamento lancamento,
+			HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoEvents(this, response, lancamentoSalvo.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+
+	@ExceptionHandler({ PessoaInativaException.class })
+	public ResponseEntity<?> handlePessoaInexistenteOuInativaException(PessoaInativaException ex) {
+		
+		String msgUser = messageSource.getMessage("pessoa.inativa", null, LocaleContextHolder.getLocale());
+		String msgDev = ex.toString();
+		List<Erro> erro = Arrays.asList(new Erro(msgUser, msgDev));
+		return ResponseEntity.badRequest().body(erro);
+	}
+	
+	@ExceptionHandler({ PessoaInexistenteException.class })
+	public ResponseEntity<?> handlePessoaInexistenteException(PessoaInexistenteException ex) {
+		
+		String msgUser = messageSource.getMessage("pessoa.inexistente", null, LocaleContextHolder.getLocale());
+		String msgDev = ex.toString();
+		List<Erro> erro = Arrays.asList(new Erro(msgUser, msgDev));
+		return ResponseEntity.badRequest().body(erro);
 	}
 }
---
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,35 @@
+package com.example.algamoney.api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInativaException;
+import com.example.algamoney.api.service.exception.PessoaInexistenteException;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@Service
+public class LancamentoService {
+
+	@Autowired
+	LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	PessoaRepository pessoaRepository;
+
+	public Lancamento salvar(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
+		if (pessoa.isEmpty()) {
+			throw new PessoaInexistenteException();
+		}
+		if(pessoa.get().isInativo()) {
+			throw new PessoaInativaException();
+		}
+		return lancamentoRepository.save(lancamento);
+	}
+}
